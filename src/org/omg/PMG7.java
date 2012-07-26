c9cd451377a5a47aa3cd74e664fbc6ee34bc5965
mmajid <mmajid@gmail.com>
2012-07-26 16:33:06 +0200

    some optimizations: making fields final

--- src/org/omg/PMG7.java
+++ src/org/omg/PMG7.java
@@ -1,6 +1,7 @@
 package org.omg;
 
 import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -20,17 +21,21 @@ import org.openscience.cdk.tools.SaturationChecker;
  * @author mahdi
  */
 public class PMG7{
-	/**Output File containing the list of graph. */
+	/**Output File containing the list of molecules. */
 	static BufferedWriter outFile;
 	private final static ForkJoinPool forkJoinPool = new ForkJoinPool();
 	
 	private static class Generator extends RecursiveTask<Long> {
 		final MolHelper2 mol;
-		static SaturationChecker satCheck = new SaturationChecker();
+		final static SaturationChecker satCheck = new SaturationChecker();
+		final boolean wFile;
+		final int nH;
 
-		public Generator(final MolHelper2 mol) {
+		public Generator(final MolHelper2 mol, final boolean wf, final int nh) {
 			super();
 			this.mol = mol;
+			this.wFile = wf;
+			this.nH = nh;
 		}
 
 		@Override
@@ -40,14 +45,14 @@ public class PMG7{
 				if (mol.isComplete(satCheck, nH) && mol.isConnected()) {
 //					if (!molSet.add(mol.canString)) System.err.println("Duplicate");
 					count = 1;
-					if(PMG.wFile){
+					if(wFile){
 						mol.writeMol(outFile);
 					}
 				}
 				ArrayList<MolHelper2> extMolList = mol.addOneBond();
 				List<Generator> tasks = new ArrayList<>();
 				for (final MolHelper2 child : extMolList) {
-					tasks.add(new Generator(child)); 
+					tasks.add(new Generator(child, wFile, nH)); 
 				}
 				for(final Generator task : invokeAll(tasks)) { 
 					count += task.join(); 
@@ -62,31 +67,23 @@ public class PMG7{
 		
 	}
 	
-	long mol_counter = 0;
-	boolean parallelExecution = true;
-
-	static int nH;
-	static boolean wFile = false;
 
 	// TODO: This is only for checking duplicates. This should be unnecessary in a good version.
 	private Set<String> molSet = Collections.newSetFromMap(new ConcurrentHashMap<String,Boolean>());
 
-	private static MolHelper2 initialize(String formula, String fragments, String output) {
+	static int hCount;
+	private static MolHelper2 initialize(String formula, String fragments) {
 		MolHelper2 mol;
 		try {
 			mol = new MolHelper2();
 
 			System.out.println("PMG7: Fork-join processing of "+formula+ " started:");
-			if (fragments == null)
-				nH = mol.initialize(formula);
-			else 
-				nH = mol.initialize(formula, fragments);
+			hCount = mol.initialize(formula, fragments);
 			for (IAtom atom:mol.acontainer.atoms()) System.out.print(atom.getSymbol());
 			System.out.println();
 			return mol;
 		} catch (IOException e) {
-			System.err.println("Could not open "+output+" for writing. Continuting without file output...");
-			wFile = false;
+			System.err.println("Could not read the fragments.");
 			e.printStackTrace();
 		} catch (CDKException e) {
 			e.printStackTrace();
@@ -96,18 +93,25 @@ public class PMG7{
 		return null;	// upon unsuccessful initialization
 	}
 
-	public static void main(String[] args) throws IOException{
+	public static void main(String[] args){
 		// parse the command-line arguments
 		String formula = null;
 		String fragments = null;
-		String out = "default_out.sdf";
+		boolean wFile = false;
 		for(int i = 0; i < args.length; i++){
 			if(args[i].equals("-mf")){
 				formula = args[++i];
 			}
 			else if(args[i].equals("-o")){
-				out = args[++i];
-				wFile = true;
+				String out = args[++i];
+				try {
+					outFile = new BufferedWriter(new FileWriter(out));
+				} catch (IOException e) {
+					System.err.println("Could not open "+out+" for writing.");
+					e.printStackTrace();
+					System.exit(10);
+				}
+				wFile  = true;
 			}
 			else if(args[i].equals("-fr")){
 				System.err.println("No fragments currently supported in the parallel MG.");
@@ -119,16 +123,15 @@ public class PMG7{
 			System.exit(0);
 		}
 		
-		MolHelper2 mol = initialize(formula, fragments, out);
+		MolHelper2 mol = initialize(formula, fragments);
 		
 		// do the real processing
 		long before = System.currentTimeMillis();
-		long finalCount = forkJoinPool.invoke(new Generator(mol));
+		long finalCount = forkJoinPool.invoke(new Generator(mol, wFile, hCount));
 		long after = System.currentTimeMillis();		
 
 		// Report the number of generated molecules
 		System.out.println("molecules " + finalCount);
-//		System.out.println("N=3 " + N3.get());
 		System.out.println("Duration: " + (after - before) + " milliseconds\n");
 	}
 
